80ea63392d5877314543617e49539e6097d393dd
Fogarasy Roland <fogarasy.roland@gmail.com>
2015-05-01 16:09:41 +0200

    rapid chat is ready!

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,33 @@
  * Controller of the firebaseTestApp
  */
 angular.module('firebaseTestApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $firebaseArray) {
+
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    var ref = new Firebase("https://glaring-inferno-8842.firebaseio.com/");
+
+
+    $scope.typing = function(e){
+      if (e.which == 13){
+        $scope.messages.$add({
+          text: $scope.messageInp
+        });
+        $scope.messageInp = "";
+      }
+    }
+
+    // $scope.data = $firebaseObject(ref);
+
+    $scope.messages = $firebaseArray(ref);
+
+    //syncObject.$bindTo($scope, "data");
+
+    $scope.sayHello = function(){
+      console.log($scope.messages);
+    }
   });
