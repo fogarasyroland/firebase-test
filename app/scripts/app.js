b032b865fd592331ea0a55ef76c36436a9cfa3b5
Fogarasy Roland <fogarasy.roland@gmail.com>
2015-05-01 20:54:12 +0200

    Colorize :)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,8 +17,26 @@ angular
     'ngSanitize',
     'ngTouch',
     'firebase'
-  ])
-  .config(function ($routeProvider) {
+  ]).filter('colorize', function() {
+    return function(input) {
+      var sum = 0;
+      var static_colors =
+       ["#0A95BF", "#B54474", "#F5863C", "#DBB85E", "#BC3C36", "#98468F", "#44C9D7"];
+
+
+
+      if (input)
+        for (var i = 0, len = input.length; i < len; i++) {
+          sum += input[i].charCodeAt(0);
+        }
+
+      //console.log(sum);
+      return static_colors[sum%static_colors.length];
+      //return input ? '\u2713' : '\u2718';
+    };
+  })
+  .config(function ($routeProvider,$sceProvider) {
+    $sceProvider.enabled(false);
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
