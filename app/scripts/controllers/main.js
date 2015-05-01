48bfe4ebee276ca87d0bfaf175fa7b78620f6707
Fogarasy Roland <fogarasy.roland@gmail.com>
2015-05-01 16:43:48 +0200

    add anonym auth

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -19,9 +19,29 @@ angular.module('firebaseTestApp')
     var ref = new Firebase("https://glaring-inferno-8842.firebaseio.com/");
 
 
+    function authDataCallback(authData) {
+      if (authData) {
+        $scope.uid = authData.uid;
+        console.log("User " + authData.uid + " is logged in with " + authData.provider);
+      } else {
+        console.log("User is logged out");
+        ref.authAnonymously(function(error, authData) {
+          if (error) {
+            console.log("Login Failed!", error);
+          } else {
+            console.log("Authenticated successfully with payload:", authData);
+          }
+        },{remember: "sessionOnly"})
+      }
+    }
+
+    ref.onAuth(authDataCallback);
+
+
     $scope.typing = function(e){
       if (e.which == 13){
         $scope.messages.$add({
+          uid: $scope.uid,
           text: $scope.messageInp
         });
         $scope.messageInp = "";
